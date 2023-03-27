4f3efd914cd2cfcb0b12938efe8d4e489dd7bd0b
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-03-27 18:48:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -62,6 +62,7 @@
     canvas.getContext("2d").drawImage(video, 0, 0);
     img.src = canvas.toDataURL("image/png");
     screenshotsContainer.prepend(img);
+    screenshotsContainer.download(img);
   });
 
   // switch camera
