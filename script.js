adfb32cbfce147d47ccac8ed6cb3af780c98c42c
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-03-27 21:22:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,9 +21,12 @@
   const constraints = {
     video: {
       width: {
-        min: 1280,
-        ideal: 1920,
-        max: 2560,
+        //min: 1280,
+        //ideal: 1920,
+        //max: 2560,
+        min: 720,
+        ideal: 1080,
+        max: 1440,
       },
       height: {
         min: 720,
